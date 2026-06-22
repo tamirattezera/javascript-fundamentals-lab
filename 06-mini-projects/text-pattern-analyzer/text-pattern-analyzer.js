a34df1012317194b3756c4449f3fcfa4674cce88
Tamirat Tezera <tamirattezera8@gmail.com>
2026-06-22 23:30:24 +0300

    feat: implement non-palindrome index detection

--- 06-mini-projects/text-pattern-analyzer/text-pattern-analyzer.js
+++ 06-mini-projects/text-pattern-analyzer/text-pattern-analyzer.js
@@ -3,3 +3,15 @@ function isPalindrome(word) {
 
   return normalizedWord === [...normalizedWord].reverse().join("");
 }
+
+function findPalindromeBreaks(words) {
+  const breaks = [];
+
+  for (const [index, word] of words.entries()) {
+    if (!isPalindrome(word)) {
+      breaks.push(index);
+    }
+  }
+
+  return breaks;
+}
